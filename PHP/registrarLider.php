685b52063e31978bdaeef419830a48c287591575
The-Mandalor-Fett <davidc.cortes@outlook.com>
2022-05-27 15:04:35 -0500

    Modificamos el inicio de sesión

--- PHP/registrarLider.php
+++ PHP/registrarLider.php
@@ -6,7 +6,7 @@ $primerApe = $_POST["primerApe"];
 $segundoApe = $_POST["segundoApe"];
 $correo = $_POST["correo"];
 $contrasena = md5($_POST["contrasena"]);
-$rol = $_POST[1];
+
 
 #Lider
 $sqlCheckLider = "SELECT * FROM lider WHERE id_lider = '$boleta'";
@@ -16,15 +16,15 @@ if(mysqli_num_rows($resCheckLider) == 1){
     <a href='./proyecto1.html'>Regresar</a>";
 }
 else{
-$sqlInsLider = "INSERT INTO lider (id_lider,nombre,pApellido,sApellido,correo,contrasena) VALUES('$boleta','$nombre','$primerApe','$segundoApe','$correo','$contrasena')";
+$sqlInsLider = "INSERT INTO lider (id_lider,nombre,pApellido,sApellido,correo) VALUES('$boleta','$nombre','$primerApe','$segundoApe','$correo')";
 $resInsLider = mysqli_query($conexion, $sqlInsLider);
-$sqlInsLiderContrasena = "INSERT INTO usuario (id,contrasena,id_rol) VALUES('$boleta','$contrasena','$rol')";
+$sqlInsLiderContrasena = "INSERT INTO usuario (id,contrasena,id_rol) VALUES('$boleta','$contrasena','1')";
 $resInsLiderContrasena = mysqli_query($conexion, $sqlInsLiderContrasena);
 if(mysqli_affected_rows($conexion) == 1){
-    echo "<h5>Se agregó correctamente el registro a la BD.<br><a href='./proyecto1.html'>Regresar</a></h5>";
+    echo "<h5>Se agregó correctamente el registro a la BD.<br><a href='/proyecto1.html'>Regresar</a></h5>";
 }
 else{
-    echo "<h5>Error. No se  pudo agregar el registro a la BD.<br>Favor de intentarlo nuevamente<br><a href='./proyecto1.html'>Regresar</a></h5>";
+    echo "<h5>Error. No se  pudo agregar el registro a la BD.<br>Favor de intentarlo nuevamente<br><a href='/proyecto1.html'>Regresar</a></h5>";
 }
     }   
 ?>
