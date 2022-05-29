238254b639027e44564a60782e153d1ee98b9517
The-Mandalor-Fett <davidc.cortes@outlook.com>
2022-05-29 01:34:45 -0500

    Agregamos sweet alert

--- JS/validacion.js
+++ JS/validacion.js
@@ -1,4 +1,24 @@
-$(document).ready(()=>{
-    $("#formAlumno").validetta();
-    $("#formSesion").validetta();
+$(document).ready(function(){
+    
+    $("form#formSesion").validetta({
+        onValid:(e)=>{
+            e.preventDefault();
+            $.ajax({
+                method:"post",
+                url:"./PHP/inicioSesion_AX.php",
+                data: $("form#formSesion").serialize(),
+                cache:false,
+                success:(RespuestaServidor_AX)=>{
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Datos correctos',
+                        text: RespuestaServidor_AX
+                      });
+                    
+
+                }
+            }); 
+        }
+    });
 });
+
--- PHP/inicioSesion_AX.php
+++ PHP/inicioSesion_AX.php
@@ -1,4 +1,28 @@
-<?
+<?php
+
+include("./conexion.php");
 $usuario = $_POST["usuario"];
-echo "Tu usuario es: - $usuario";
+$contrasena = md5($_POST["contrasena"]);
+
+
+$sqlCheckUsr = "SELECT * FROM usuario WHERE id = '$usuario' AND contrasena = '$contrasena' AND id_rol = '1'";
+$sqlCheckADMIN = "SELECT * FROM usuario WHERE id = '$usuario' AND contrasena = '$contrasena' AND id_rol = '0' ";
+$resCheckUsr = mysqli_query($conexion, $sqlCheckUsr);
+$resCheckADMIN = mysqli_query($conexion, $sqlCheckADMIN);
+if(mysqli_num_rows($resCheckUsr) == 1){
+    $_SESSION["login"] = true;
+    //header("location:./proyecto.php");
+    echo "Tus datos están registrados.Alumno";
+}
+elseif(mysqli_num_rows($resCheckADMIN) == 1){
+    $_SESSION["login"] = true;
+    //header("location:./admin.php"); 
+    echo "Tus datos están registrados.Alumno";
+    
+}
+else{
+    echo "Tus datos no están registrados. Favor de registrarse.";
+    //<a href='/tadw/proyecto1.html'>Regresar</a>;
+    }
+
 ?>
